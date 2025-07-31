81ef100cfef8f89fac2a21c8cd942f298bc852c9
Pooja-chaudhary960 <pujadahit52@gmail.com>
2025-07-31 18:14:12 +0545

    Initial commit

--- src/pages/Admin/DoctorsList.jsx
+++ src/pages/Admin/DoctorsList.jsx
@@ -18,9 +18,10 @@ const DoctorsList = () => {
       <h1 className='text-lg font-medium'>All Doctors</h1>
       <div className='w-full flex flex-wrap gap-4 pt-5 gap-y-6'>
         {
-          doctors.map((item,index)=>(
-            <div className='border border-indigo-200 rounded-xl max-w-56 overflow-hidden cursor-pointer group' key={index}>
-              <img  className='bg-indigo-50 group-hover:bg-blue-500 transition-all duration-500 block'src={item.image} alt='Doctors images'/>
+          doctors.map((item)=>(
+            <div className='border border-indigo-200 rounded-xl max-w-56 overflow-hidden cursor-pointer group' key={item._id}>
+              <img className='bg-indigo-50 group-hover:bg-blue-500 transition-all duration-500 block' src={item.image} alt='Doctors images'/>
+
               <div className='p-4'>
                 <p className='text-neutral-800'>{item.name}</p>
                 <p className='text-zinc-600 text-sm'>{item.speciality}</p>
